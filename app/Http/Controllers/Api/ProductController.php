0f41c23f9835543e238c3687f47f5f041dd8229c
hasan <hasanmurai56@gmail.com>
2026-04-07 22:11:51 +0300

    change on deleteAccount

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
