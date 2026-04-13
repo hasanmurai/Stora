52d0700157d92a2f8767fbb41e48a6301a511c18
hasan <hasanmurai56@gmail.com>
2026-04-13 21:09:30 +0300

    fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\{Shop,Product};
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
