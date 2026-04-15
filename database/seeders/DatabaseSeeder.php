d9b34ed0a4da15c8dda42e80bb19c0f09f352a75
hasan <hasanmurai56@gmail.com>
2026-04-15 23:04:46 +0300

    create test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $cat = \App\Models\Category::create(['name' => 'Electronics', 'slug' => 'electronics']);
         // User::factory(10)->create();
         \Illuminate\Support\Facades\Artisan::call('passport:client', [
         '--personal' => true,
         '--name' => 'Laravel Personal Access Client',
         '--no-interaction' => true,
     ]);
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
         'name' => 'Owner User',
         'email' => 'owner@gmail.com',
         'password' => bcrypt('12345678'),
         'role' => 'owner',
     ]);
+        \App\Models\User::create([
+
+        'name' => 'User',
+        'email' => 'user1@gmail.com',
+        'password' => bcrypt('12345678'),
+        'role' => 'user',
+    
+        ]);
+        \App\Models\User::create([
+
+        'name' => 'User',
+        'email' => 'user2@gmail.com',
+        'password' => bcrypt('12345678'),
+        'role' => 'user',
+    
+        ]);
     }
 }
